b9c2dbf1d315a4b1088383590d4c2dbf84ac6606
pioncore <info@pioncoin.org>
2019-02-09 23:42:59 +0530

    Pionize travis osx build scripts

--- contrib/dash/travis/travis-build-osx.sh
+++ contrib/dash/travis/travis-build-osx.sh
@@ -6,13 +6,13 @@ if [[ -z $TRAVIS_TAG ]]; then
   exit 1
 fi
 
-BUILD_REPO_URL=https://github.com/akhavr/electrum-dash.git
+BUILD_REPO_URL=https://github.com/pioncoin/electrum-pion.git
 
 cd build
 
-git clone --branch $TRAVIS_TAG $BUILD_REPO_URL electrum-dash
+git clone --branch $TRAVIS_TAG $BUILD_REPO_URL electrum-pion
 
-cd electrum-dash
+cd electrum-pion
 
 export PY36BINDIR=/Library/Frameworks/Python.framework/Versions/3.6/bin/
 export PATH=$PATH:$PY36BINDIR
@@ -22,7 +22,7 @@ echo wine build version is $DASH_ELECTRUM_VERSION
 sudo pip3 install --upgrade pip
 sudo pip3 install -r contrib/deterministic-build/requirements.txt
 sudo pip3 install \
-    x11_hash>=1.4 \
+    x16r-hash==1.0.1 \
     pycryptodomex==3.6.1 \
     btchip-python==0.1.27 \
     keepkey==4.0.2 \
@@ -42,15 +42,15 @@ cp contrib/dash/pyi_tctl_runtimehook.py .
 
 pyinstaller \
     -y \
-    --name electrum-dash-$DASH_ELECTRUM_VERSION.bin \
+    --name electrum-pion-$DASH_ELECTRUM_VERSION.bin \
     osx.spec
 
-info "Adding Dash URI types to Info.plist"
+info "Adding Pion URI types to Info.plist"
 plutil -insert 'CFBundleURLTypes' \
-   -xml '<array><dict> <key>CFBundleURLName</key> <string>dash</string> <key>CFBundleURLSchemes</key> <array><string>dash</string></array> </dict></array>' \
-   -- dist/Dash\ Electrum.app/Contents/Info.plist \
+   -xml '<array><dict> <key>CFBundleURLName</key> <string>pion</string> <key>CFBundleURLSchemes</key> <array><string>pion</string></array> </dict></array>' \
+   -- dist/Pion\ Electrum.app/Contents/Info.plist \
    || fail "Could not add keys to Info.plist. Make sure the program 'plutil' exists and is installed."
 
-sudo hdiutil create -fs HFS+ -volname "Dash Electrum" \
-    -srcfolder dist/Dash\ Electrum.app \
-    dist/Dash-Electrum-$DASH_ELECTRUM_VERSION-macosx.dmg
+sudo hdiutil create -fs HFS+ -volname "Pion Electrum" \
+    -srcfolder dist/Pion\ Electrum.app \
+    dist/Pion-Electrum-$DASH_ELECTRUM_VERSION-macosx.dmg
